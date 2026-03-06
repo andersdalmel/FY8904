68b833c7f6a47cd526682b31c2164d8e2ca8699c
Anders Daltveit Melve <andersdm@ntnu.no>
2026-03-06 16:29:08 +0100

    New proj

--- Project2/SFDmain.py
+++ Project2/SFDmain.py
@@ -2,5 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-print(np.e)
+def convertRepresentation(image, N, M):
+    if len(image.shape) > 1: #if the image is in a matrix representation
+        vectorRep = np.zeros(N*M)
+        for i in range(N):
+            for j in range(M):
+                vectorRep[M*i + j] = image[i, j]
+        return vectorRep
+    elif len(image.shape) == 0:
+        matrixRep = np.zeros((N, M))
+        for i in range(N*M):
+            matrixRep[, ] = image[i]
